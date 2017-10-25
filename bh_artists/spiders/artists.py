bdc03f2083451647fa8a7aa729ae0f00ed84917b
wojciezki <wojciechjakubiak@gmail.com>
2017-10-25 12:57:40 +0200

    unnecessary lines deleted

--- bh_artists/spiders/artists.py
+++ bh_artists/spiders/artists.py
@@ -1,8 +1,4 @@
-import re
-import urllib.parse
 import scrapy
-from scrapy.spiders import CrawlSpider
-from scrapy.linkextractors import LinkExtractor
 from bh_artists.items import BhArtistsItem
 
 
@@ -12,8 +8,6 @@ class ArtistSpider(scrapy.Spider):
     start_urls = ['http://berghain.de/events/']
 
     def parse(self, response):
-        # import pdb
-        # pdb.set_trace()
         # create array of auctions links
         event_page = response.xpath('//*[@class="x3cols"]/div/h4/span/a/@href').extract()
         events = []
@@ -34,13 +28,9 @@ class ArtistSpider(scrapy.Spider):
             yield request
 
         for div in response.xpath('//div[@class="expandlist"]//a/@href'):
-            # import pdb
-            # pdb.set_trace()
             yield response.follow(div, callback=self.parse)
 
     def parse_event(self, response):
-        # import pdb
-        # pdb.set_trace()
         item = response.meta['item']
 
         item['event_date'] = response.xpath('//div[@class="headline"]/h2/text()').extract_first()
@@ -51,7 +41,8 @@ class ArtistSpider(scrapy.Spider):
         for row in items:
 
             item['lineup'] = \
-                [x.strip() for x in row.xpath('//span[@class="running_order_name"]/text()[normalize-space()]').extract()]
+                [x.strip() for x in row.xpath('//span[@class="running_order_name"]/text()[normalize-space()]')
+                    .extract()]
 
         yield item
 
